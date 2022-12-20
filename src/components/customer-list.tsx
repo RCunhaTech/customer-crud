3e290c7599d000a660ca1610cde99f25cdb65a36
Roberto Cunha <88341832+RCunhaTech@users.noreply.github.com>
2022-12-20 17:23:44 -0300

    create componte customerList

---
+++ src/components/customer-list.tsx
@@ -0,0 +1,7 @@
+export function CustomerList(props:any) {
+    return (
+        <div className="container">
+            <h2>{ props.name }</h2>
+        </div>
+    )
+}
--- src/home/App.tsx
+++ src/home/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
+import { CustomerList } from '../components/customer-list'
 import './App.css'
 
 function App() {
@@ -8,18 +9,22 @@ function App() {
     name:string
   }
 
-  const [customers, setCustomers] = useState<Customer[]>([])
+  const [customers, setCustomers] = useState<Customer[]>([{
+    name:'Roberto'
+  }])
 
   return (
-    <div className="App">
-      <h2>Registro de Clientes</h2>
-      <input 
-        type="text" 
-        placeholder="digite o nome">
-      </input>
-
-      <button>Adicionar</button>
-    </div>
+    <>
+      <div className="App">
+        <h2>Registro de Clientes</h2>
+        <input 
+          type="text" 
+          placeholder="digite o nome">
+        </input>
+        <button>Adicionar</button>
+      </div>
+      <CustomerList name="Roberto" />
+    </>
   )
 }
 
